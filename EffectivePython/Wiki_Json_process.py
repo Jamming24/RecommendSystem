476531748ebc3f876d6a8ee97ed0a0cfeb11cf01
Jamming <gaojiaming24@163.com>
2019-07-31 13:52:35 +0800

    2019-7-31 维基百科处理程序

---
+++ EffectivePython/Wiki_Json_process.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/31 9:47
+# @Author  : Jamming
+# @Email   : gaojiaming24@163.com
+# @File    : Wiki_Json_process.py
+# @Software: PyCharm
+
+import json
+import codecs
+from tqdm import tqdm
+
+"""
+本程序功能是将json形式的维基百科数据进一步进行简繁体转换和拆句处理
+"""
+
+
+def load_json(wiki_json):
+    print('load file', wiki_json)
+    text_list = []
+    error_line = []
+    # 使用编码方式读取文件
+    f = codecs.open(wiki_json, 'r', encoding='UTF-8')
+    # tqdm 是一个可扩展的进度条
+    for line in tqdm(f.readlines()):
+        try:
+            entity = json.loads(line.strip())
+            text = entity['text'].replace('\n', '')
+            # id = entity['id']
+            # url = entity['url']
+            # title = entity['title']
+            text_list.append(text)
+        except:
+            error_line.append(line)
+            continue
+    print(len(error_line))
+    # 丢弃了121行异常数据
+    return text_list
+
+
+def Split_Sentence(text_list, sentence_wiki):
+    # 将文章拆分为带有标点符号的句子
+    file = open(sentence_wiki, 'w', encoding='UTF-8')
+    writer_line = []
+    for line in text_list:
+        sentences = line.split("。")
+        for ss in sentences:
+            if len(ss) > 0:
+                writer_line.append(ss)
+    print(f"总计有{len(writer_line)}行数据")
+    for w in tqdm(writer_line):
+        file.write(w+'\n')
+    file.close()
+
+
+def Simplified_Conversion():
+    # 将繁体中文转换为简体中文
+    # 使用OpenCC进行转换 使用命令行 不需要代码
+    pass
+
+
+root_qi = "E:/Teacher_Qi/"
+wiki_json = root_qi + "ZhWiki_extractor/zh_wiki_data.txt"
+sentence_wiki = root_qi + "zh_wiki_sentence_data.txt"
+text_list = load_json(wiki_json)
+Split_Sentence(text_list, sentence_wiki)
+
+
+
+
+
